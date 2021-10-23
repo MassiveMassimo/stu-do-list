0fd7d0a3634c76d8938c3edcd8abc003b177efe7
Imo Madjid <71184274+MassiveMassimo@users.noreply.github.com>
2021-10-23 12:25:42 +0000

    Trying to fix navbar script

--- schedule_kuliah/models.py
+++ schedule_kuliah/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+Kode_Matkul = [
+  ("Alin"),
+  ("Basdat")
+  ("Manpro"),
+  ("MPPI"),
+]
+
+class Matakuliah (models.Model):
+  kode = models.CharField(max_length=10)
+  kelas = models.CharField(max_length=1)
+  jadwal = models.
