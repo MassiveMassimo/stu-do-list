a827a0d89dec576f216a2dfda47704f2f3f8dc68
Imo Madjid <71184274+MassiveMassimo@users.noreply.github.com>
2021-10-27 05:17:00 +0000

    Added stuff to notes

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Notes(models.Model):
+  nama = models.CharField(max_length=50)
+  kelas = models.CharField(max_length=15)
+  prodi = models.CharField(max_length=50)
+  info = models.CharField(max_length=100)
+  link = models.URLField()
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Notes
+from django.http.response import HttpResponseRedirect
+from django.contrib.auth.decorators import 
 
-# Create your views here.
+def index(request):
+  notes = Notes.objects.all()
+  response = {'notes': notes}
+  return render(request, 'notes_index.html', response)
+
+@login_required(login_url="/admin/login/")
+def add_notes(request):
+  form = NotesForm(request.POST or None)
+  if (form.is_valid() and request.method == 'POST'):
+    form.save()
+    return HttpResponseRedirect('/notes/')
+  context ={'form':form}
+  return render(request, "notes_form.html", context)
