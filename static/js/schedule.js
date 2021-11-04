518e347d7597075610b07df309a90c434421d246
jennytjhio <jenny@ui.ac.id>
2021-11-05 02:05:32 +0700

    fix styling

--- static/js/schedule.js
+++ static/js/schedule.js
@@ -42,8 +42,14 @@ $.get(`/schedule-kuliah/get-jadwal/${userId}`, function (data) {
                 <td>${jadwal.fields.hari}</td>
                 <td>${jadwal.fields.start}</td>
                 <td>${jadwal.fields.end}</td>
-                <td>
-                    <a class="btn btn-outline-danger" href="/schedule-kuliah/delete-jadwal/${jadwal.pk}">delete</a>
+                <td style="width:2%;">
+                    <a class="y-0 e-0 s-0 text-sm text-gray-500 whitespace-no-wrap border-b border-gray-200" href = "/schedule-kuliah/delete-jadwal/${jadwal.pk}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-400 m-auto" fill="none"
+                        viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                    </a>
                 </td>
             </tr>
             `
@@ -55,3 +61,7 @@ $.get(`/schedule-kuliah/get-jadwal/${userId}`, function (data) {
 
     $("#table-jadwal").html(html)
 })
+
+{/* <td>
+                    <a class="btn btn-outline-danger" href="/schedule-kuliah/delete-jadwal/${jadwal.pk}">delete</a>
+                </td> */}
