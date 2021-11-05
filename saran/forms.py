5d5ccea369677eae4d63519695531a24bfda2063
zachra shafira <zachra.shafira@ui.ac.id>
2021-11-05 21:22:19 +0700

    saran forms views modified + nambah url saran + ubah jadwal belajar dikit

--- saran/forms.py
+++ saran/forms.py
@@ -6,7 +6,7 @@ class SaranForm(forms.ModelForm):
         model = Saran
         fields = "__all__"
         widgets = {
-            'Nama': forms.TextInput(attrs={'type':'text', 'class':'form-control', 'placeholder':'Masukkan nama / inisial Anda'}),
-            'Teks': forms.Textarea(attrs={'type':'text', 'class':'form-control', 'placeholder':'Masukkan saran'}),
-            'Email': forms.EmailInput(attrs={'type':'email', 'class':'form-control', 'placeholder':'Masukkan email Anda'})
+            'Nama': forms.TextInput(attrs={'type':'text', 'class':'form-control', 'placeholder':'Masukkan nama atau inisial Anda'}),
+            'Email': forms.EmailInput(attrs={'type':'email', 'class':'form-control', 'placeholder':'Masukkan email Anda'}),
+            'Teks': forms.Textarea(attrs={'type':'text', 'class':'form-control', 'placeholder':'Masukkan saran'})
         }
