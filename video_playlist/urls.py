51f0788ce6d67403d539fd10ecf1ad95ecaad613
chalika.vanya <chalikavanyaresya.2015@gmail.com>
2021-12-29 15:18:15 +0700

    untuk mobile app

--- video_playlist/urls.py
+++ video_playlist/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index, add_video
+from .views import get_playlist, index, add_video, flutter_project
 
 urlpatterns = [
   path('', index, name='index'),
   path('add-video', add_video, name='add'),
+  path('video-flutter', flutter_project, name='video'),
+  path('get-playlist', get_playlist, name='get'),
 ]
--- video_playlist/views.py
+++ video_playlist/views.py
@@ -1,8 +1,11 @@
-from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from .models import Video
 from .forms import VideoForm
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.core import serializers
 
 # Create your views here.
 def index(request):
@@ -29,3 +32,25 @@ def add_video(request):
 
   response['form']= form
   return render(request, "video_playlist_form.html", response)
+
+@csrf_exempt
+def flutter_project(request):
+  if request.method == 'POST':
+        videoPlaylist = json.loads(request.body)
+
+        new_video = Video(
+            Title = videoPlaylist['title'],
+            Link = videoPlaylist['link'],
+        )
+
+        new_video.save()
+        return JsonResponse({"instance": "Video berhasil ditambahkan!"}, status=200)
+
+def get_playlist(request):
+    video_playlist = Video.objects.all()
+    data = serializers.serialize('json', video_playlist)
+    response = {
+            'video_playlist':data,
+            }
+    return JsonResponse(response)
+
