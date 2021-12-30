a7e8a64d3bb98d2e4f0b901c00cbe158ec025b60
zachra shafira <zachra.shafira@ui.ac.id>
2021-12-31 00:57:00 +0700

    ganti models

--- jadwal_belajar_bareng/forms.py
+++ jadwal_belajar_bareng/forms.py
@@ -1,9 +1,5 @@
 from django import forms
 from .models import JadwalBelajarBareng
-import datetime
-
-class DateTimeInput(forms.DateTimeInput):
-    input_type: 'datetime'
 
 class JadwalForm(forms.ModelForm):
     class Meta:
@@ -12,7 +8,7 @@ class JadwalForm(forms.ModelForm):
         widgets = {
             'Prioritas': forms.Select(attrs={'type':'text', 'id': 'inputPrioritas', 'class':'form-control'}),
             'Matkul': forms.Select(attrs={'type':'text', 'id': 'inputMatkul', 'class':'form-control'}),
-            'Waktu': forms.DateTimeInput(attrs={'type':'datetime-local', 'class':'form-control'}),
+            'Waktu': forms.TextInput(attrs={'type':'text', 'id': 'inputDate', 'class':'form-control', 'placeholder':'Masukkan waktu pembelajaran'}),
             'Topik': forms.TextInput(attrs={'type':'text', 'class':'form-control', 'placeholder':'Masukkan topik pembelajaran'}),
             'Informasi': forms.Textarea(attrs={'type':'text', 'class':'form-control', 'placeholder':'Masukkan informasi tambahan'}),
             'Link': forms.URLInput(attrs={'type':'url', 'class':'form-control', 'placeholder':'Masukkan situs pertemuan daring'})
