c5ff09da09bae4b8eb7756a7b1aa541507feaea9
MassiveMassimo <muhammad.hannan@ui.ac.id>
2021-12-31 21:24:40 +0700

    Trying to turn user field to username instead of int

--- ask_a_mentor/serializers.py
+++ ask_a_mentor/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from .models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(slug_field='username')
     class Meta:
         model = Post
         fields = ('id', 'title', 'user', 'matkul', 'message', 'time') 
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(slug_field='username')
     class Meta:
         model = Comment
         fields = ('id', 'post', 'user', 'comment', 'time') 
