a296dee9c0f1b8e7cc0d8ccd1f2930cd83c434eb
dimas-id <dimas.ilham01.ui.ac.id>
2021-12-31 14:37:19 +0700

    update on agenda - kelewatan urls

--- agenda/urls.py
+++ agenda/urls.py
@@ -6,6 +6,7 @@ app_name = "agenda"
 urlpatterns = [
     path('', index, name='index'),
     path('add', add_agenda, name='add'),
+    path('flutter-add', flutter_add, name='flutter-add'),
     path('get', get_agenda, name='get'),
     # path('get/<str:user_id>', get_agenda, name='get'),
     path('delete/<str:agenda_id>', delete_agenda, name='delete'),
