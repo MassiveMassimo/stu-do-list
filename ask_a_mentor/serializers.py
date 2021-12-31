cd7bf32e51379aedfb291c14abc7c86109a14d74
MassiveMassimo <muhammad.hannan@ui.ac.id>
2021-12-31 21:29:38 +0700

    Fixed some errors

--- ask_a_mentor/serializers.py
+++ ask_a_mentor/serializers.py
@@ -2,14 +2,16 @@ from rest_framework import serializers
 from .models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
-    user = serializers.SlugRelatedField(slug_field='username')
+    read_only = True
+    slug_field='username'
     class Meta:
         model = Post
         fields = ('id', 'title', 'user', 'matkul', 'message', 'time') 
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.SlugRelatedField(slug_field='username')
+    read_only = True
+    slug_field='username'
     class Meta:
         model = Comment
         fields = ('id', 'post', 'user', 'comment', 'time') 
