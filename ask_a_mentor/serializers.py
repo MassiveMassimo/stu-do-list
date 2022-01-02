5ace902199d20532dc01fd39d5cd7e4101d26d93
MassiveMassimo <muhammad.hannan@ui.ac.id>
2022-01-02 17:45:07 +0700

    Set default user in Ask a Mentor

--- ask_a_mentor/serializers.py
+++ ask_a_mentor/serializers.py
@@ -13,5 +13,4 @@ class CommentSerializer(serializers.ModelSerializer):
     username = serializers.CharField( source="user.username", read_only=True)
     class Meta:
         model = Comment
-        fields = '__all__' 
         exclude = ('user', )
